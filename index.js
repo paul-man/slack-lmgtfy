df8e11b4bb404b12ea6ed6ed31ab4286a7ed067c
paul-man <paul.mannarino@stonybrook.edu>
2017-05-08 01:22:23 -0400

    POST

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Route that receives a POST request to /sms
 app.post('/lmgtfy', function (req, res) {
-    var obj = req.client.socket;
+    var obj = req.client;
     res.set('Content-Type', 'text/plain');
     console.log("Post");
     console.log(obj);
