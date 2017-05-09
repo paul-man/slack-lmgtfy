ef76942972642a22f08b0b6605d0b9d5db859c34
paulman <paul.mannarino@stonybrook.edu>
2017-05-09 02:16:46 -0400

    Added attatchment response

--- index.js
+++ index.js
@@ -22,18 +22,32 @@ app.post('/lmgtfy', function (req, res) {
     var keys = Object.keys(obj);
     console.log('obj contains ' + keys.length + ' keys: '+  keys);
 
+    // {
+    //     "text": "It's 80 degrees right now.",
+    //     "attachments": [
+    //         {
+    //             "text":"Partly cloudy today and tomorrow"
+    //         }
+    //     ]
+    // }
     // res.send(util.inspect(obj, {depth: null}));
     var link = "https://www.google.com/#q=" + encodeURIComponent(text);
 
-    res.set('Content-Type', 'text/plain');
-    res.send(link);
+    res.writeHead(200, {"Content-Type": "application/json"});
+    var attachments_arr = [{text: link}];
+    // var otherObject = { item1: "item1val", item2: "item2val" };
+    var json = JSON.stringify({
+        text: "Here's your link, lazy bastard!",
+        attachments: attachments_arr
+    });
+    response.end(json);
 });
 
 // Tell our app to listen on port 3000
 app.listen(port, function (err) {
-  if (err) {
-    throw err;
-  }
+    if (err) {
+        throw err;
+    }
 
-  console.log('Server started on port', port);
+    console.log('Server started on port', port);
 });
