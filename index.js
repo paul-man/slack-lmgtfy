e0bc7e6b67c4177145495bfbf0f4f786ee3c77d0
paul-man <paul.mannarino@stonybrook.edu>
2017-05-09 23:53:33 -0400

    adding youtube lookup

--- index.js
+++ index.js
@@ -46,6 +46,34 @@ app.post('/lmgtfy', function (req, res) {
     res.end(json);
 });
 
+app.post('/youtube', function (req, res) {
+    var obj = req.client._httpMessage.req.body;
+    var text = obj.text;
+    console.log("Post");
+    console.log(obj);
+    // res.send(util.inspect(obj, {depth: null}));
+
+    // {
+    //     "text": "It's 80 degrees right now.",
+    //     "attachments": [
+    //         {
+    //             "text":"Partly cloudy today and tomorrow"
+    //         }
+    //     ]
+    // }
+
+    var link = "https://www.youtube.com/results?search_query=" + encodeURIComponent(text);
+
+    res.writeHead(200, {"Content-Type": "application/json"});
+    var attachments_arr = [{text: link}];
+    var json = JSON.stringify({
+        response_type: "in_channel",
+        text: "Here's your link, lazy!",
+        attachments: attachments_arr
+    });
+    res.end(json);
+});
+
 // Tell our app to listen on port 3000
 app.listen(port, function (err) {
     if (err) {
@@ -60,9 +88,9 @@ setInterval(function() {
     var hour = moment().hour();
     var minute = moment().minutes();
     if (day >= 1 && day <= 5) {
-        if ((hour >= 23 && minute >=45 )  && (hour <= 24)) {
-            console.log(moment().format(), "DEBUG : Making sure heroku is awake.");
+        if ((hour >= 9)  && (hour <= 18)) {
+            console.log("DEBUG : Making sure heroku app is awake::", moment().format());
             https.get("https://slack-lmgtfy.herokuapp.com");
         }
     }
-}, 3000); // every 5 minutes (300000)
+}, 1500000); // every 5 minutes (300000)
